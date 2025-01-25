0f63ef23d0a1ae9dc8042d5cb4cffb4e88c7d985
unknown <stefano.benassai.1@gmail.com>
2025-01-25 15:07:13 +0100

    init

---
+++ src/service/housing.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { HousingService } from './housing.service';
+
+describe('HousingService', () => {
+  let service: HousingService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HousingService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
+
+export class HousingLocation {
+}
